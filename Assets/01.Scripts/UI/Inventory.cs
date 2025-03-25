4677ffca945fcbc4aaad1e0fa43d27529da2c207
ssolem <tjrals961@gmail.com>
2025-03-25 13:31:31 +0900

    [feat] item 인벤토리에 표시

--- Assets/01.Scripts/UI/Inventory.cs
+++ Assets/01.Scripts/UI/Inventory.cs
@@ -1,20 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField] private List<ItemData> items;
+    [SerializeField] private UISlot slotPrefab;
+    [SerializeField] private RectTransform slotParent;
 
-    public List<ItemData> Items { get; private set; } 
+    public List<ItemData> Items { get { return items; } }
+
+    public List<UISlot> slots = new List<UISlot>();
 
     void Start()
     {
-        
+        Init();
     }
 
-    void Update()
+    void Init()
     {
-        
+        for (int i = 0; i < 40; i++)
+        {
+            UISlot newSlot = Instantiate(slotPrefab, slotParent);
+            slots.Add(newSlot);
+        }
+
+        for (int i = 0; i < items.Count; i++)
+        {
+            if (items[i] != null)
+            {
+                slots[i].GetItemData(items[i]);
+            }
+        }
     }
 }
--- Assets/01.Scripts/UI/UISlot.cs
+++ Assets/01.Scripts/UI/UISlot.cs
@@ -1,8 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UISlot : MonoBehaviour
 {
     private ItemData itemData;
+
+    [SerializeField] private Image icon;
+    [SerializeField] private Button slotButton;
+
+    private void Start()
+    {
+        slotButton.onClick.AddListener(OnClickSlotButton);
+    }
+
+    public void GetItemData(ItemData data)
+    {
+        itemData = data;
+        SetSlot();
+    }
+
+    void SetSlot()
+    {
+        icon.sprite = itemData.Icon;       
+    }
+
+    public void ClearSlot()
+    {
+        icon.sprite = null;
+        itemData = null;
+    }
+
+    void OnClickSlotButton()
+    {
+
+    }
 }
